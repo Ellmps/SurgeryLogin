ff574592656cff6efe9f778ae05d1f98e7ca7550
Ellmps <56725014+Ellmps@users.noreply.github.com>
2020-03-13 12:12:38 +0000

    Add files via upload

--- SurgeryLogin/Form1.Designer.cs
+++ SurgeryLogin/Form1.Designer.cs
@@ -33,12 +33,13 @@
             this.btnLogin = new System.Windows.Forms.Button();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblUser
             // 
             this.lblUser.AutoSize = true;
-            this.lblUser.Location = new System.Drawing.Point(144, 93);
+            this.lblUser.Location = new System.Drawing.Point(250, 122);
             this.lblUser.Name = "lblUser";
             this.lblUser.Size = new System.Drawing.Size(58, 13);
             this.lblUser.TabIndex = 0;
@@ -47,7 +48,7 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(147, 153);
+            this.lblPassword.Location = new System.Drawing.Point(253, 182);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(56, 13);
             this.lblPassword.TabIndex = 1;
@@ -55,32 +56,43 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(293, 227);
+            this.btnLogin.Location = new System.Drawing.Point(380, 246);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(94, 37);
+            this.btnLogin.Size = new System.Drawing.Size(120, 56);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(274, 85);
+            this.txtUsername.Location = new System.Drawing.Point(380, 114);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(100, 20);
             this.txtUsername.TabIndex = 3;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(274, 153);
+            this.txtPassword.Location = new System.Drawing.Point(380, 182);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(100, 20);
             this.txtPassword.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.Location = new System.Drawing.Point(318, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Surgery Login";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.btnLogin);
@@ -100,6 +112,7 @@
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
     }
 }
 
